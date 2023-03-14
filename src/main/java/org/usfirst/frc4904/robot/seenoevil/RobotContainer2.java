11cae5e0f7b80f15ecc1e752df239a4a5b0a19b5
Exr0n <mail@exr0n.com>
2023-03-13 22:38:08 -0700

    need to reverse the headings on reverse trajectory
    as per https://docs.wpilib.org/en/stable/docs/software/advanced-controls/trajectories/trajectory-generation.html#creating-the-trajectory-config

--- src/main/java/org/usfirst/frc4904/robot/seenoevil/RobotContainer2.java
+++ src/main/java/org/usfirst/frc4904/robot/seenoevil/RobotContainer2.java
@@ -91,9 +91,9 @@ public class RobotContainer2 {
                         trajectoryConfig
                 )),
                 entry("straight_backward", TrajectoryGenerator.generateTrajectory(
-                        new Pose2d(0, 0, new Rotation2d(0)),
+                        new Pose2d(0, 0, new Rotation2d(Math.PI)),
                         List.of(new Translation2d(1, 0)),
-                        new Pose2d(2, 0, new Rotation2d(0)),
+                        new Pose2d(2, 0, new Rotation2d(Math.PI)),
                         trajectoryConfigReversed
                 ))
         );
