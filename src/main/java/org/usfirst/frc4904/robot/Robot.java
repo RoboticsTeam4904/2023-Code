f23fa01f42007091a82f96efb7a5b3f24becd312
roboticsteam4904-2 <botprovoking@gmail.com>
2023-03-15 19:01:26 -0700

    Fix it later

--- src/main/java/org/usfirst/frc4904/robot/Robot.java
+++ src/main/java/org/usfirst/frc4904/robot/Robot.java
@@ -12,6 +12,11 @@ import java.util.TimerTask;
 import org.usfirst.frc4904.robot.humaninterface.drivers.NathanGain;
 import org.usfirst.frc4904.robot.humaninterface.operators.DefaultOperator;
 import org.usfirst.frc4904.standard.CommandRobotBase;
+import org.usfirst.frc4904.standard.custom.motorcontrollers.CANTalonFX;
+import org.usfirst.frc4904.standard.subsystems.motor.TalonMotorSubsystem;
+
+import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 
 import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.controller.DifferentialDriveWheelVoltages;
@@ -20,43 +25,50 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 
 // TODO implement test and sim in CommandRobotBase
 public class Robot extends CommandRobotBase {
-    private RobotMap map = new RobotMap();
+    private final CANTalonFX lefta = new CANTalonFX(1, InvertType.None);
+    private final CANTalonFX leftb = new CANTalonFX(2, InvertType.None);
+    private final TalonMotorSubsystem leftMotors = new TalonMotorSubsystem("yes", NeutralMode.Brake, 10, lefta, leftb);
+
+    // private RobotMap map = new RobotMap();
 
     @Override
     public void initialize() {
-        driverChooser.setDefaultOption(new NathanGain());
-        operatorChooser.setDefaultOption(new DefaultOperator());
+        // driverChooser.setDefaultOption(new NathanGain());
+        // operatorChooser.setDefaultOption(new DefaultOperator());
         // autoChooser.setDefaultOption(RobotMap.Autonomous.autonCommand);  // zach is worried that this will get misclicked -> screw us
     }
 
     @Override
     public void teleopInitialize() {
-        var controller = RobotMap.HumanInput.Driver.xbox;
-        var maxSpeed = 12;
-        RobotMap.Component.chassis.leftMotors.setBrakeOnNeutral();
-        RobotMap.Component.chassis.rightMotors.setBrakeOnNeutral();
+        // var controller = RobotMap.HumanInput.Driver.xbox;
+        // var maxSpeed = 12;
+        // RobotMap.Component.chassis.leftMotors.setBrakeOnNeutral();
+        // RobotMap.Component.chassis.rightMotors.setBrakeOnNeutral();
 
-        // RobotMap.Component.chassis.setDefaultCommand(RobotMap.Component.chassis.c_controlChassisSpeedAndTurn(() -> new Pair<Double, Double>(Robot.drivingConfig.getX(), Robot.drivingConfig.getTurnSpeed())));
-        RobotMap.Component.chassis.c_controlWheelVoltages(() -> new DifferentialDriveWheelVoltages(
-            (controller.getRightTriggerAxis() - controller.getLeftTriggerAxis() + 1*controller.getLeftX())*maxSpeed,
-            (controller.getRightTriggerAxis() - controller.getLeftTriggerAxis() - 1*controller.getLeftX())*maxSpeed
-        )).schedule();
+        // // RobotMap.Component.chassis.setDefaultCommand(RobotMap.Component.chassis.c_controlChassisSpeedAndTurn(() -> new Pair<Double, Double>(Robot.drivingConfig.getX(), Robot.drivingConfig.getTurnSpeed())));
+        // RobotMap.Component.chassis.c_controlWheelVoltages(() -> new DifferentialDriveWheelVoltages(
+        //     (controller.getRightTriggerAxis() - controller.getLeftTriggerAxis() + 1*controller.getLeftX())*maxSpeed,
+        //     (controller.getRightTriggerAxis() - controller.getLeftTriggerAxis() - 1*controller.getLeftX())*maxSpeed
+        // )).schedule();
     }
 
     @Override
     public void teleopExecute() {
-        System.out.println(RobotMap.Component.chassis.leftMotors.leadMotor.get());
+        // System.out.println(RobotMap.Component.chassis.leftMotors.leadMotor.get());
     }
 
     @Override
     public void autonomousInitialize() {
         // TODO: remove; for testing only
-        RobotMap.Component.chassis.leftMotors.setBrakeOnNeutral();
-        RobotMap.Component.chassis.rightMotors.setBrakeOnNeutral();
+        // RobotMap.Component.chassis.leftMotors.setBrakeOnNeutral();
+        // RobotMap.Component.chassis.rightMotors.setBrakeOnNeutral();
+
     }
 
     @Override
     public void autonomousExecute() {
+        leftMotors.set(-0.3);
+
     }
 
     @Override
